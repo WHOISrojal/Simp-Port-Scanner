bfe45f6981f751ef21ffbbab8420f58c36e412df
WHOISrojal <rojal2002@gmail.com>
2023-08-07 18:21:39 +0545

    Changes

---
+++ portscanner.sh
@@ -0,0 +1,17 @@
+#Simple Port Scanner
+
+#Syntax:
+#nmap <type of scan> <Target IP address> <Optional, Target Port> <write destination>
+
+# /dev/null = This write destination tells to send the output not in the screen but to make it disappear.
+# -oG= This is used inorder to use grep command at the time of cat. 
+
+
+Code:
+#!/bin/bash
+nmap -sT 192.168.1.1 -p 443 /dev/null -oG PortScanner
+cat PortScanner | grep open > PortScanner2
+cat PortScanner2
+
+
+
