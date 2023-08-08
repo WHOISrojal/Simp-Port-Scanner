12e6e3ecbd9223501497a260f3860bf42fa58993
WHOISrojal <rojal2002@gmail.com>
2023-08-08 16:14:35 +0545

    Update Info

--- portscanner.sh
+++ portscanner.sh
@@ -13,5 +13,6 @@ nmap -sT 192.168.1.1 -p 443 /dev/null -oG PortScanner
 cat PortScanner | grep open > PortScanner2
 cat PortScanner2
 
+#Kindly wait for some time to get a little advanced scanner to be uploaded.
 
 
